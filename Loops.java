c42e0352dd9a3d763e522ac28928d9c44782a2c0
VovaKor <volodymyr.korobko@gmail.com>
2017-11-02 22:54:33 +0200

    Create Loops.java

---
+++ Loops.java
@@ -0,0 +1,138 @@
+package com.korobko;
+
+/**
+ *
+ * @author vova
+ */
+public class Loops {
+
+public static void main(String[] args) {
+int shapeSize=10;   
+    /*
+   Isosceles triangle
+                          * 
+                        *   * 
+                      *       * 
+                    *           * 
+                  *               * 
+                *                   * 
+              *                       * 
+            *                           * 
+          *                               * 
+        *   *   *   *   *   *   *   *   *   * 
+*/
+
+                   for (int j=0; j<(shapeSize-1); j++)  System.out.print(" ");
+    System.out.print("* ");
+    System.out.print("\n");
+                   for (int j=1; j<shapeSize-1; j++)
+                        {
+                            for (int k=0;k<(shapeSize-1-j); k++) System.out.print(" ");
+                            System.out.print("* ");
+                            for (int k=0;k<(j*2)-2; k++) System.out.print(" ");        
+                            System.out.print("* ");
+                            System.out.print("\n");
+                        }
+    for (int j=0; j<shapeSize; j++) System.out.print("* ");
+
+System.out.print("\n");
+System.out.print("\n");
+/*
+ Right triangle
+                    * 
+                    * * 
+                    *   * 
+                    *     * 
+                    *       * 
+                    *         * 
+                    *           * 
+                    *             * 
+                    *               * 
+                    * * * * * * * * * * 
+*/
+           
+    System.out.print("* ");
+    System.out.print("\n");
+           for (int j=1; j<(shapeSize-1); j++)
+                        {
+                            System.out.print("* ");
+                            for (int k=0;k<(j*2)-2; k++) System.out.print(" ");        
+                            System.out.print("* ");
+                            System.out.print("\n");
+                        }
+          for (int j=0; j<shapeSize; j++) System.out.print("* ");
+    System.out.print("\n");
+    System.out.print("\n");
+/*
+Rectangle
+          * * * * * * * * * * * * * * * * * * * * 
+          *                                     * 
+          *                                     * 
+          *                                     * 
+          *                                     * 
+          *                                     * 
+          *                                     * 
+          *                                     * 
+          *                                     * 
+          * * * * * * * * * * * * * * * * * * * * 
+*/
+                 
+        for (int j=0; j<shapeSize*2; j++) System.out.print("* ");
+System.out.print("\n");
+        for (int j=1; j<(shapeSize-1); j++)
+                        {                           
+                            System.out.print("* ");
+                            for (int k=0;k<(shapeSize*4-4); k++) System.out.print(" ");        
+                            System.out.print("* ");
+                            System.out.print("\n");
+                        }
+        for (int j=0; j<shapeSize*2; j++) System.out.print("* ");
+System.out.print("\n");
+System.out.print("\n");
+/*
+Rhombus
+                       * 
+                      * * 
+                     *   * 
+                    *     * 
+                   *       * 
+                  *         * 
+                 *           * 
+                *             * 
+               *               * 
+              *                 * 
+               *               * 
+                *             * 
+                 *           * 
+                  *         * 
+                   *       * 
+                    *     * 
+                     *   * 
+                      * * 
+                       * 
+*/
+
+                   for (int j=0; j<(shapeSize-1); j++)  System.out.print(" ");
+    System.out.print("* ");
+    System.out.print("\n");
+                   for (int j=1; j<shapeSize-1; j++)
+                        {
+                            for (int k=0;k<(shapeSize-1-j); k++) System.out.print(" ");
+                            System.out.print("* ");
+                            for (int k=0;k<(j*2)-2; k++) System.out.print(" ");        
+                            System.out.print("* ");
+                            System.out.print("\n");
+                        }
+                   for (int j=shapeSize-1; j>0; j--)
+                        {
+                            for (int k=0;k<(shapeSize-1-j); k++) System.out.print(" ");
+                            System.out.print("* ");
+                            for (int k=0;k<(j*2)-2; k++) System.out.print(" ");        
+                            System.out.print("* ");
+                            System.out.print("\n");
+                        }
+    for (int j=0; j<(shapeSize-1); j++)  System.out.print(" ");
+    System.out.print("* ");
+
+}
+}
